fb0c62533364334cd8eb60e79b9921b308844e4f
KauiLebarbenchon <151573133+KauiLebarbenchon@users.noreply.github.com>
2024-07-14 15:03:09 -0400

    Update doctor.py

--- doctor.py
+++ doctor.py
@@ -4,3 +4,11 @@ class Doctor:
 
         self.q = dequeue()
         self.rate = a
+
+
+
+class Patient:
+    def __init__(self, budget, disease, urgency):
+        self.budget = budget
+        self.disease = disease
+        self.urgency = urgency
